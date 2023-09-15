b3fef967a2d16fd4e7e27ea1a945c8e28b37ac3d
maria salcedo <mayi13sa02@hotmail.com>
2023-09-15 13:25:13 -0500

    media query 480px

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { renderItems } from './view.js';
 
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
+import pokemon from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, renderItems, data);
@@ -11,6 +12,9 @@ const formNombre = document.getElementById('formNombre');
 const contenedorCards = document.querySelector('.contenedor-cards');
 const imagen = document.getElementById('imagen');
 const footer = document.querySelector('footer');
+const pokemonCadaCards = document.querySelector('root li');
+
+
 
 /*  ocultar el form imagen y footer*/
 formNombre.addEventListener('submit', (event) => {
@@ -38,4 +42,27 @@ formNombre.addEventListener('submit', handleFormSubmit);
    document.getElementById("root").style.display = "block";
    imagen.style.display = 'none';
 });
+
+
+/*traer el pokemon*/
+function crearCards() {
+  fetch("pokemon.json")
+    .then(respuesta => respuesta.json())
+    .then(pokemon => {
+      const pokemonCadaCards = document.getElementById("root");
+      
+      pokemon.forEach(pokemon => {
+        const poke = document.createElement("tr");
+        poke.innerHTML = `
+          <td>${pokemon.num}</td>
+          <td>${pokemon.name}</td>
+          <td>${pokemon.type.join(", ")}</td>
+        `;
+        
+        pokemonCadaCards.appendChild(poke);
+      });
+    });
+}
+
+crearCards();
 
