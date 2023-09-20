a86b6cff7f911e0a5b471c0a5ca0f1817e93df74
Kotte91 <kottevaldebenito@gmail.com>
2023-09-19 21:26:15 -0300

    div.numero/nombre/tipo,confirmar antes de aplicar

--- src/view.js
+++ src/view.js
@@ -7,10 +7,16 @@ export const renderItems = (data) => {
     const li = document.createElement("li");
     const img = document.createElement("img");
     const p = document.createElement("p");
+    const n = document.createElement("n");
+    const t = document.createElement ("t");
     img.src = pokemon.img;
-    p.textContent = `${pokemon.num}  ${pokemon.name}${pokemon.type}`;
+    p.textContent = `${pokemon.num}`
+    n.textContent = `${pokemon.name}`
+    t.textContent = `${pokemon.type}`
     li.appendChild(img);
     li.appendChild(p)
+    li.appendChild(n)
+    li.appendChild(t)
     Tarjetas.appendChild(li);
   });
   return Tarjetas;
