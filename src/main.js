ce6444ff41bf3761b1d14154848bdf5f5f9b1ef4
Kotte91 <kottevaldebenito@gmail.com>
2023-09-20 13:37:48 -0300

    cards arregladar y cambio de header img, falta h1

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { renderItems } from './view.js';
 
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-import pokemon from './data/pokemon/pokemon.js';
+//import pokemon from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, renderItems, data);
@@ -20,7 +20,7 @@ contenedorTarjetas.replaceWith(prueba)
 
 
 
-
+contenedorTarjetas.style.display = 'none'
 
 /*  ocultar el form imagen y footer*/
 formNombre.addEventListener('submit', (event) => {
@@ -33,20 +33,18 @@ formNombre.addEventListener('submit', (event) => {
   localStorage.setItem('nombreUsuario', nombreUsuario);
   formNombre.style.display = 'none';
   contenedorCards.style.display = 'block';
-  imagen.style.display = 'none'; 
-  footer.style.display = 'none'; 
+  imagen.style.display = 'none';
+  footer.style.display = 'none';
   mostrarCards();
 });
 /*formNombre.addEventListener('submit', handleFormSubmit);*/
-    
-     
 
-     /*  colocar el nombre y sale mensaje de alerta */
-    
-   /*ocultar li , ul,imagen */
-   /*document.getElementById("inicio").addEventListener("click", function() {
-   document.getElementById("root1").style.display = "block";
-   imagen.style.display = 'none';
+/*  colocar el nombre y sale mensaje de alerta */
+
+/*ocultar li , ul,imagen */
+/*document.getElementById("inicio").addEventListener("click", function() {
+  document.getElementById("root1").style.display = "block";
+  imagen.style.display = 'none';
 });*/
 
 
--- src/view.js
+++ src/view.js
@@ -2,13 +2,13 @@
 
 export const renderItems = (data) => {
   /*return 'example';*/
-  const Tarjetas= document.createElement("ul");
+  const Tarjetas = document.createElement("ul");
   data.forEach(pokemon => {
     const li = document.createElement("li");
     const img = document.createElement("img");
     const p = document.createElement("p");
     const n = document.createElement("n");
-    const t = document.createElement ("t");
+    const t = document.createElement("t");
     img.src = pokemon.img;
     p.textContent = `${pokemon.num}`
     n.textContent = `${pokemon.name}`
