16c7deebe0b7a1c817a4bb338586eed9d176f0cb
maria salcedo <mayi13sa02@hotmail.com>
2023-09-29 09:29:27 -0500

    hicimos un test de filtro

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -6,16 +6,19 @@ console.log(fakeData);
 describe( filterData, () => {
 
   it('deberia darnos el tipo de pokemon "posion"' + fakeData + '"', () => {
-    const selectFilter = document.querySelector('#filtro');
-    //const resultFilter =filterData(fakeData,'type',"poison")
-    filterData (fakeData);
-    expect(filtro.innertext).toBe(poison);
+    //const selectFilter = document.querySelector('#filtro');
+    const resultFilter = filterData(fakeData, 'type', 'poison');
+    
+    // VerificaR que resultFilter contenga solo Pokémon de tipo "poison"
+    resultFilter.forEach(pokemon => {
+      expect(pokemon.type).toBe('poison');
+    });
   });
-});
+})
 
-describe('anotherExample', () => {
+// describe('anotherExample', () => {
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
